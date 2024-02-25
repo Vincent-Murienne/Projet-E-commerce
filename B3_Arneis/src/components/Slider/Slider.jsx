5db65cec5574ad62fc8202c2fffc5eecdc5e7864
Fantastix80 <jean.vanwittenberge@gmail.com>
2024-02-25 16:12:06 +0100

    <Feat>(panel admin): Branche fonctionnel.

--- B3_Arneis/src/components/Slider/Slider.jsx
+++ B3_Arneis/src/components/Slider/Slider.jsx
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { getData } from '../../services/api';
 
-// test modif
-
 export default function Slider() {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [getSliderImages, setSliderImages] = useState([]);
