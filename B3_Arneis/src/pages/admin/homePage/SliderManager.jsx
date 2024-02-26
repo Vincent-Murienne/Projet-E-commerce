8d2643f0cae278932c43e31b74be2bbbf30ee4c5
jean <jean.vanwittenberge@gmail.com>
2024-02-26 20:47:22 +0100

    <Feat>(panel admin): Mise en place de l'arborescence du panel admin.

---
+++ B3_Arneis/src/pages/admin/homePage/SliderManager.jsx
@@ -0,0 +1,16 @@
+
+
+const SliderManager = () => {
+    return(
+        <>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h1>Slider manager</h1>
+        </>
+    );
+};
+
+export default SliderManager;
