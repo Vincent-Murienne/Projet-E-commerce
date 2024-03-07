a0e36bf03ffa884f5162a10854de1c68d29db59f
brassica <brassica943@gmail.com>
2024-03-07 18:13:51 +0100

    Feat<categorie>: modification de la description

--- B3_Arneis/src/pages/categoriePage/CategoriePage.jsx
+++ B3_Arneis/src/pages/categoriePage/CategoriePage.jsx
@@ -13,21 +13,23 @@ const CategoriePage = () => {
         <img src="/img/1-1-chaise-design-blanc-et-bois-clair-.jpg" alt="Chaise" className="categoriePage-image" />
     </div>
      
-     <div className="description-centre">
+    <div className="description-centre">
     <h1>Bienvenue dans la boutique "Àrneis" de la catégorie Chaise:</h1>
-    <p>
-        Découvrez notre sélection de chaises alliant style et confort pour votre espace de vie. <br /> <br />
-        Commandez ici vos nouvelles chaises d'intérieur. Il y en a pour tous les goûts, profitez de produits originaux très colorés qui sauront
-        sublimer votre décoration. <br /><br />
-        Passez commande sur notre site et recevez vos chaises ! Explorez dès aujourd'hui notre collection pour trouver la chaise idéale qui reflète votre style et votre personnalité.
-    </p>
-</div>
+        <p>
+            Quels que soient vos préférences, vous trouverez des produits originaux et colorés qui rehausseront votre décoration.
+        </p>
+        <p>
+            Passez commande dès maintenant sur notre site et recevez vos chaises chez vous !
+        </p>
+        
+        <h2>Découvrez ici notre gamme de chaises d'intérieur :</h2>
+    </div>
 
 <div className="card-container">
     <div className="card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <img src="/img/1-3-chaise-design-blanc-et-bois-clair-.jpg" className="card-img-top" alt="..." />
         <div className="card-body">
-        <button className="btn2">Acheter maintenant</button>  
+        <button className="btn2">Voir le produit</button>  
         </div>
         <div className="card-title">            
             <h4>Chaise en bois clair</h4>
@@ -37,7 +39,7 @@ const CategoriePage = () => {
 <div className="card">
     <img src="/img/2-2-chaise-pivotante.jpg" className="card-img-top" alt="..." />
     <div className="card-body">
-    <button className="btn2">Acheter maintenant</button>
+    <button className="btn2">Voir le produit</button>
     </div>
 
     <div className="card-title">
@@ -48,7 +50,7 @@ const CategoriePage = () => {
     <div className="card">
         <img src="/img/3-3-Chaise en métal .jpg" className="card-img-top" alt="..." />
         <div className="card-body">
-            <button className="btn2">Acheter maintenant</button>      
+            <button className="btn2">Voir le produit</button>      
         </div>
 
         <div className="card-title">
@@ -59,7 +61,7 @@ const CategoriePage = () => {
     <div className="card">
         <img src="/img/4-1-Chaise de bar.jpg" className="card-img-top" alt="..." />
         <div className="card-body">
-    <button className="btn2">Acheter maintenant</button>
+    <button className="btn2">Voir le produit</button>
         </div>
         <div className="card-title">
             <h4>Chaise de bar </h4>
