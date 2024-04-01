5f91946ddb98bb1d1a7f255e9894c79489e760b5
brassica <brassica943@gmail.com>
2024-04-01 18:11:33 +0200

    <Feat>(categorie): Récuperation du nom des categories

--- Back-end/config/db.php
+++ Back-end/config/db.php
@@ -129,8 +129,8 @@ class Database {
 
     public function getProductsFromCategory($id)
     {
-        $sql = "SELECT products.*, product_image.category_id AS product_image_category_id, product_image.name AS product_image_name, category_image.category_id AS category_image_category_id, category_image.name AS category_image_name FROM products INNER JOIN (SELECT product_id, MIN(id) AS min_image_id FROM images GROUP BY product_id) AS first_image ON products.id = first_image.product_id INNER JOIN images AS product_image ON first_image.min_image_id = product_image.id LEFT JOIN images AS category_image ON products.category_id = category_image.category_id AND category_image.category_id = :id WHERE products.category_id = :id ORDER BY products.quantity DESC";
-
+    
+        $sql = "SELECT categories.name AS category_name, products.*, product_image.category_id AS product_image_category_id, product_image.name AS product_image_name, category_image.category_id AS category_image_category_id, category_image.name AS category_image_name FROM products INNER JOIN (SELECT product_id, MIN(id) AS min_image_id FROM images GROUP BY product_id) AS first_image ON products.id = first_image.product_id INNER JOIN images AS product_image ON first_image.min_image_id = product_image.id LEFT JOIN images AS category_image ON products.category_id = category_image.category_id AND category_image.category_id = :id LEFT JOIN categories ON products.category_id = categories.id WHERE products.category_id = :id ORDER BY products.quantity DESC";
         $query = $this->pdo->prepare($sql);
         $query->bindValue("id", $id, PDO::PARAM_INT);
         $query->execute();
