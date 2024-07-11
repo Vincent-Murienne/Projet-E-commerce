033cd7e3cb2638fa5c604dfa4948a754158b4c3f
Brassica <brassica.selvaratnam@gmail.com>
2024-07-11 16:00:28 +0200

    <Feat>(android_categories_liste): Mise en place du Design de la page liste Categories

---
+++ B3_Airneis_Mobile/app/src/main/java/com/example/airneis/fragment/CategoriesAdapter.java
@@ -0,0 +1,61 @@
+package com.example.airneis.fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.airneis.R;
+import com.example.airneis.model.Category;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoriesAdapter.ViewHolder> {
+
+    private List<Category> categories;
+
+    public CategoriesAdapter(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_category, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Category category = categories.get(position);
+        holder.categoryName.setText(category.getName());
+        Picasso.get().load("http://10.0.2.2:8000/img/" + category.getImageName()).into(holder.categoryImage);
+        holder.voirPlus.setOnClickListener(v -> {
+            // Handle button click, you can add navigation to category details here
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView categoryImage;
+        TextView categoryName;
+        Button voirPlus;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            categoryImage = itemView.findViewById(R.id.categoryImage);
+            categoryName = itemView.findViewById(R.id.categoryName);
+            voirPlus = itemView.findViewById(R.id.voirPlus);
+        }
+    }
+}
---
+++ B3_Airneis_Mobile/app/src/main/java/com/example/airneis/model/Category.java
@@ -0,0 +1,27 @@
+package com.example.airneis.fragment;
+/*package com.example.airneis.model;*/
+
+
+public class Category {
+    private int id;
+    private String name;
+    private String imageName;
+
+    public Category(int id, String name, String imageName) {
+        this.id = id;
+        this.name = name;
+        this.imageName = imageName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+}
