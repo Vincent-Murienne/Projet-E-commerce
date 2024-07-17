7afa54245deabe5c50a3de01d50eef8d54818ae7
Brassica <brassica.selvaratnam@gmail.com>
2024-07-18 00:31:17 +0200

    <Feat>(android_category_liste):Affichages de la liste des categories est terminée

--- B3_Airneis_Mobile/app/src/main/java/com/example/airneis/fragment/CategoriesFragment.java
+++ B3_Airneis_Mobile/app/src/main/java/com/example/airneis/fragment/CategoriesFragment.java
@@ -1,80 +1,93 @@
 package com.example.airneis.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.airneis.R;
+import com.example.airneis.adapter.TopCategoriesAdapter;
+import com.example.airneis.model.TopCategoriesData;
 import com.example.airneis.manager.APIManager;
-import com.example.airneis.model.CategoryData;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class CategoriesFragment extends Fragment {
-
     private RecyclerView recyclerView;
-    private CategoriesAdapter adapter;
-    private List<CategoryData> categories;
+    private TopCategoriesAdapter categoriesAdapter;
+    private ArrayList<TopCategoriesData> topCategoriesList;
 
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_categories, container, false);
-
         recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        categories = new ArrayList<>();
-        adapter = new CategoriesAdapter(categories);
-        recyclerView.setAdapter(adapter);
+        topCategoriesList = new ArrayList<>();
 
-        fetchCategories();
+        // Set GridLayoutManager with 3 columns
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
 
+        categoriesAdapter = new TopCategoriesAdapter(topCategoriesList);
+        recyclerView.setAdapter(categoriesAdapter);
+
+        fetchTopCategories();
         return view;
     }
 
-    private void fetchCategories() {
+    private void fetchTopCategories() {
         APIManager apiManager = new APIManager(getContext());
-        try {
-            apiManager.apiCall("category", "getAllCategories", new JSONObject(), new APIManager.VolleyResponseListener() {
-                @Override
-                public void onError(String message) {
-                    Toast.makeText(getContext(), "Erreur : " + message, Toast.LENGTH_SHORT).show();
-                }
 
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        if (response.getBoolean("success")) {
-                            JSONArray data = response.getJSONArray("data");
-                            for (int i = 0; i < data.length(); i++) {
-                                JSONObject categoryJson = data.getJSONObject(i);
-                                CategoryData category = new CategoryData(
-                                        categoryJson.getInt("category_id"),
-                                        categoryJson.getString("category_name"),
-                                        categoryJson.getString("image_name")
-                                );
-                                categories.add(category);
-                            }
-                            adapter.notifyDataSetChanged();
-                        } else {
-                            Toast.makeText(getContext(), "Erreur : " + response.getString("error"), Toast.LENGTH_SHORT).show();
+        JSONObject data = new JSONObject();
+        try {
+            data.put("table", "categories");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        APIManager.VolleyResponseListener listener = new APIManager.VolleyResponseListener() {
+            @Override
+            public void onError(String message) {
+                Log.e("API Error", message);
+            }
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("API Response", response.toString());
+                try {
+                    if (response.getBoolean("success")) {
+                        JSONArray categories = response.getJSONArray("data");
+                        for (int i = 0; i < categories.length(); i++) {
+                            JSONObject category = categories.getJSONObject(i);
+                            String imageName = category.getString("image_name");
+                            String categoryName = category.getString("category_name");
+                            int categoryId = category.getInt("category_id");
+                            topCategoriesList.add(new TopCategoriesData(categoryId, categoryName, imageName));
                         }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                        categoriesAdapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(getContext(), response.getString("error"), Toast.LENGTH_SHORT).show();
                     }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            });
+            }
+        };
+
+        try {
+            apiManager.apiCall("category", "getAllCategories", data, listener);
         } catch (JSONException e) {
             e.printStackTrace();
         }
